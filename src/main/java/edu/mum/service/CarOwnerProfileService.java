e6bb5d5ddfd550a0208af2a8a4bd5b455999bf62
nguyenchitam <nguyenchitam@tmac.cs.mum.edu>
2018-12-17 15:02:09 -0600

    Service

--- src/main/java/edu/mum/service/CarOwnerProfileService.java
+++ src/main/java/edu/mum/service/CarOwnerProfileService.java
@@ -8,11 +8,7 @@ import edu.mum.validation.ValidationException;
 public interface CarOwnerProfileService extends GenericService<CarOwnerProfile> {
 	public abstract List<CarOwnerProfile> findPendingApproveProfiles();
 
-	public abstract List<CarOwnerProfile> findAll();
+	public abstract void create(CarOwnerProfile profile) throws ValidationException;
 
-	public abstract CarOwnerProfile create(CarOwnerProfile profile) throws ValidationException;
-
-	public abstract CarOwnerProfile findById(long id);
-
-	public abstract CarOwnerProfile approveProfile(CarOwnerProfile profile) throws ValidationException;
+	public abstract void approveProfile(CarOwnerProfile profile) throws ValidationException;
 }
